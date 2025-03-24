45ae00b3ef634b2652409e39686e1ae31f3f3b7a
Seila Gamo <seilagamo@gmail.com>
2025-03-24 08:42:28 +0100

    Fix header in mobile

---
+++ packages/app/stylelint.config.mjs
@@ -0,0 +1,5 @@
+/** @type {import('stylelint').Config} */
+/** @type {import('stylelint').Config} */
+export default {
+  extends: ["stylelint-config-standard", "stylelint-config-rational-order"]
+};
